d3a2c7daf83fd9a8db45388439729ccfd83863ee
y1-wang <154703785+y1-wang@users.noreply.github.com>
2025-02-16 17:32:04 +0100

    Juego funcional
    en conjunto no sirve aún

--- Práctica 5/YiyiWang_Práctica5.js
+++ Práctica 5/YiyiWang_Práctica5.js
@@ -1,3 +1,5 @@
+
+/*
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('button-jugar').addEventListener('click', function() {
         document.getElementById('pantalla-inicial').classList.add('hidden');
@@ -14,9 +16,40 @@ document.getElementById('button-reiniciar').addEventListener('click', function()
     document.getElementById('pantalla-final').classList.add('hidden');
     document.getElementById('pantalla-inicial').classList.remove('hidden');
 });
+*/
 
 
-var character = document.getElementById("character");
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump(){
+    if(dino.classList != "jump"){ //no reseteará la animación una y otra vez
+        dino.classList.add("jump");
+    }
+
+    setTimeout(function() {  //elimina la clase jump, para poder volver a ejecutarlo
+        dino.classList.remove("jump");
+    }, 400);
+}
+
+let isAlive = setInterval(function(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus) . getPropertyValue("left"));
+
+    if(cactusLeft < 20 && cactusLeft > 0 && dinoTop >= 130){
+        cactus.style.animation = "none";
+        cactus.style.display = "none"; //ES NECESARIO REINICIAR SI SE USA ESTO
+        alert("Game Over!");
+    }
+}, 10);
+
+document.addEventListener("keydown", function(event){ //con cualquier tecla salta
+    jump();
+});
+
+
+
+/*
 var block = document.getElementById("block");
 
 function jump(){
@@ -41,11 +74,4 @@ var checkDead = setInterval(function(){
         }
 },10);
 
-document.getElementById("button-jugar").addEventListener("click", function() {
-    document.addEventListener("click", jump);
-    document.addEventListener("keydown", function(event){
-        if(event.code === "Space"){ 
-            jump();
-        }
-    });
-});
+*/
