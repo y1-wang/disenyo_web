fa0580465fed413148d35946717ace4b466fcca9
y1-wang <154703785+y1-wang@users.noreply.github.com>
2025-02-19 18:56:26 +0100

    Arreglo de la estructura
    para su funcionalidad óptima, falta poder reiniciar el juego

--- Práctica 5/YiyiWang_Práctica5.js
+++ Práctica 5/YiyiWang_Práctica5.js
@@ -1,24 +1,3 @@
-
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('button-jugar').addEventListener('click', function() {
-        document.getElementById('pantalla-inicial').classList.add('hidden');
-        document.getElementById('pantalla-juego').classList.remove('hidden');
-    });
-});
-
-document.getElementById('button-instrucciones').addEventListener('click', function() {
-    document.getElementById('instrucciones').classList.toggle('hidden');
-});
-
-
-document.getElementById('button-reiniciar').addEventListener('click', function() {
-    document.getElementById('pantalla-final').classList.add('hidden');
-    document.getElementById('pantalla-inicial').classList.remove('hidden');
-});
-*/
-
-
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
@@ -35,16 +14,48 @@ function jump(){
 let isAlive = setInterval(function(){
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus) . getPropertyValue("left"));
+    let botonReiniciar = document.getElementById("button-reiniciar");
 
     if(cactusLeft < 20 && cactusLeft > 0 && dinoTop >= 130){
         cactus.style.animation = "none";
         cactus.style.display = "none"; //ES NECESARIO REINICIAR SI SE USA ESTO
+        
+        botonReiniciar.classList.remove("hidden");
+
         alert("Game Over!");
     }
 }, 10);
 
 document.addEventListener("keydown", function(event){ //con cualquier tecla salta
     jump();
+});
+
+
+// Función para reiniciar el juego. COMPLETA ESTO
+function reiniciarJuego() {
+    jump(); 
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('button-jugar').addEventListener('click', function(jump) {
+        document.getElementById('pantalla-inicial').style.display="none";
+        document.getElementById('pantalla-juego').classList.remove('hidden');
+    });
+});
+
+document.getElementById('button-instrucciones').addEventListener('click', function() {
+    document.getElementById('instrucciones').classList.toggle('hidden');
+});
+
+
+document.getElementById('button-reiniciar').addEventListener('click', function() {
+    document.getElementById("pantalla-inicial").style.display = "flex";
+    document.getElementById('pantalla-juego').classList.add('hidden');
+    document.getElementById("button-reiniciar").style.display = "none";
+
+
 });
 
 
